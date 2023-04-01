45c3f7925db49969a66826b97d01f84073884db5
VineethSajjan <vineethsajjan7736@gmail.com>
2023-04-01 19:56:39 +0530

    first commit

---
+++ java,epam/second.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class second {
+    public static void main(String[] args) {
+        //Write a program, which read a String from System.in and print "Hello, <input string>"
+        Scanner sc  = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print("Hello, "+""+s);
+    }
+}
