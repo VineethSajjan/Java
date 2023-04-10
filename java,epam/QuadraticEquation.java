7c6336970b4e66559d92d3abaf9ed43772dbefbe
VineethSajjan <vineethsajjan7736@gmail.com>
2023-04-10 18:56:03 +0530

    commit

--- java,epam/QuadraticEquation.java
+++ java,epam/QuadraticEquation.java
@@ -6,7 +6,7 @@ import static java.lang.Math.sqrt;
 
 public class QuadraticEquation {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
