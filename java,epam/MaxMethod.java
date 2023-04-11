56101d8a831758815af8999fd3afa747fb9f9b19
VineethSajjan <vineethsajjan7736@gmail.com>
2023-04-11 14:50:51 +0530

    plz

--- java,epam/MaxMethod.java
+++ java,epam/MaxMethod.java
@@ -1,12 +1,18 @@
-
+//package com.epam.rd.autotasks.max;
+//import java.util.*;
 public class MaxMethod {
     public static int max(int[] values) {
         //throw new UnsupportedOperationException();
-        int m = Integer.MIN_VALUE;
+        //Scanner sc = new Scanner(System.in);
+        //int n=sc.nextInt();
+        //int arr[] = new int [n];
+        //for(int i=0;i<arr.length;i++){
+         //   arr[i]=sc.nextInt();
+        //}
+        int max=Integer.MIN_VALUE;
         for(int i:values){
-            m= Math.max(m,i);
+            max=Math.max(max,i);
         }
-        return m;
+        return max;
     }
 }
-
