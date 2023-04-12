b50fd00295db2f1fb7069c0f1030e3450e1b0ba3
VineethSajjan <vineethsajjan7736@gmail.com>
2023-04-12 21:02:16 +0530

    vin

--- java,epam/electronicWatch.java
+++ java,epam/electronicWatch.java
@@ -1,21 +1,22 @@
 
 import java.util.Scanner;
 
-class electronicWatch{
+import java.util.Scanner;
+
+class ElectronicWatch{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
         int s=seconds%60;
         int m = seconds/60;
         int h = m/60;
+        if(h==24) h=0;
         m=m%60;
         String s1 = String.format("%02d",s);
         String m1 = String.format("%02d",m);
-        if(h==24){
-            h=0;
-        }
-       // String h1 = String.format("%02d",h);
+        //String h1 = String.format("%02d",h);
         System.out.println(h+":"+m1+":"+s1);
         scanner.close();
     }
 }
+
