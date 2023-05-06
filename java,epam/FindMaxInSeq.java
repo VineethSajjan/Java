c289bd7e73a534c8127e55eb72c6c19e3a4ebab0
VineethSajjan <vineethsajjan7736@gmail.com>
2023-05-06 12:13:08 +0530

    vin

---
+++ java,epam/FindMaxInSeq.java
@@ -0,0 +1,32 @@
+
+//package com.epam.rd.autotasks.sequence;
+import java.util.*;
+
+public class FindMaxInSeq {
+    public static int max() {
+        Scanner sc = new Scanner(System.in);
+
+        int max=Integer.MIN_VALUE,v=1;
+        // Put your code here
+        while(v!=0){
+            v=sc.nextInt();
+            if(v!=0){
+                max = Math.max(max,v);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Test your code here!\n");
+
+        // Get a result of your code
+
+        //ArrayList<Integer> l = new ArrayList<>();
+
+
+
+        System.out.println(max());
+    }
+}
